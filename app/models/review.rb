9c25f17288fc76ec58d0d425fd6cd3faeaf2c8de
Mariama Sakho <mariama.sakho@gmail.com>
2026-01-14 15:06:45 +0100

    Add Restaurant and Review models with validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,5 +3,10 @@ class Review < ApplicationRecord
 
   validates :content, presence: true
   validates :rating, presence: true
-  validates :rating, numericality: { only_integer: true, greater_than_or_equal: 0, less_than_or_equal_to: 5 }
+  validates :rating,
+            numericality: {
+              only_integer: true,
+              greater_than_or_equal_to: 0,
+              less_than_or_equal_to: 5
+            }
 end
