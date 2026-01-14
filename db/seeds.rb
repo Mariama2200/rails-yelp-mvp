66fe586c5b2c7c8b39332bf59a2e6dfd6e1f95ab
Mariama Sakho <mariama.sakho@gmail.com>
2026-01-14 17:53:57 +0100

    Add restaurants and reviews MVP

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,15 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+puts "Cleaning DB..."
+Review.destroy_all
+Restaurant.destroy_all
+
+puts "creating restaurants..."
+restaurants = [
+  { name: "Chez Léon", address: "Rue de la Paix, Paris", phone_number: "01 11 22 33 44", category: "french" },
+  { name: "Sushi Zen", address: "12 avenue Sakura, Paris", phone_number: "01 22 33 44 55", category: "japanese" },
+  { name: "Roma Bella", address: "3 via Roma, Paris", phone_number: "01 33 44 55 66", category: "italian" },
+  { name: "Wok Palace", address: "8 rue Panda, Paris", phone_number: "01 44 55 66 77", category: "chinese" },
+  { name: "Bruxelles House", address: "5 rue Bière, Paris", phone_number: "01 55 66 77 88", category: "belgian" }
+]
+
+Restaurant.create!(restaurants)
+puts "Done!"
